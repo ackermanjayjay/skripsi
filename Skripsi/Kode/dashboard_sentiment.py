7fdf51b25b657d922af9bad7c7bfe635403e46f7
ackermanjayjay <reza23198@gmail.com>
2023-07-08 14:32:14 +0700

    dev bab 4

--- Skripsi/Kode/dashboard_sentiment.py
+++ Skripsi/Kode/dashboard_sentiment.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from preprocessing_text import preprocessing_text
+from sklearn.feature_extraction.text import TfidfVectorizer
+from joblib import load
+from pickle import load
+import pickle
 
 tab1, tab2 = st.tabs(["Pakaian", "Elektronik"])
 
 with tab2:
     st.header('Sentimen analisis produk tokopedia')
-    st.title('Produk Pakaian ')
+    st.title('Produk Elektronik')
 
     opini_elektornik = st.text_input('Masukkan opini ini untuk Produk Elektronik')
     model_elektronik=st.radio(
@@ -28,13 +32,24 @@ with tab1:
     st.title('Produk Pakaian ')
 
     opini_pakaian = st.text_input('Masukkan opini ini untuk Produk Pakaian')
+    data_input = preprocessing_text(opini_pakaian)
+    tfidf = TfidfVectorizer
+
+    
     model_pakaian=st.selectbox(
         "Pilih model",
         ('KNN', 'Decision tree', 'Naives bayes'))
     
     if model_pakaian=="Decision tree":
             st.write('Anda memilih Dec tree')
+            loaded_vec = TfidfVectorizer(decode_error="replace", vocabulary=set(pickle.load(open("model-tfidf\feature_tf-idf-pakaian.pkl", "rb"))))
+            
+            pipeline_dec_tree = load("model-tree\model_dec_pakaian_40persen.joblib")
+
+            hasil = pipeline_dec_tree.predict(loaded_vec.fit_transform([data_input]))
             st.write('', opini_pakaian)
+            st.write('', hasil)
+            
     if model_pakaian=="KNN":
             st.write('Anda memilih KNN')
             st.write('', opini_pakaian)
---
+++ Skripsi/Kode/preprocessing_text.py
@@ -0,0 +1,52 @@
+def caseFolding(text):
+  text=text.lower()
+  return text
+
+
+import re
+def punc_removal(text):
+  text=re.sub(r"[^a-zA-Z]"," ",text)
+  return text
+
+# import StemmerFactory class
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# create stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+def stemsWords(text):
+  text=stemmer.stem(text)
+  return text
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stp_indo=stopwords.words('indonesian')
+stp_more=["yg","tg"]
+def flatten(l):
+    """
+    Same as
+    flat_list = []
+    for sublist in l:
+    for item in sublist:
+        flat_list.append(item)
+
+    Example =[[1,2,3,4,5]] - > [1,2,3,4,5]
+    """
+    return " ".join([item for sublist in l for item in sublist])
+
+def remove_stop_words(text):
+  stp=stp_indo+stp_more
+  clean_words = []
+  text=text.split()
+  perulangan_text=[word for word in text if word not in stp]
+  clean_words.append(perulangan_text)
+  return flatten(clean_words)
+
+def preprocessing_text(text):
+  text=caseFolding(text)
+  text=punc_removal(text)
+  text=remove_stop_words(text)
+  text=stemsWords(text)
+  return text
